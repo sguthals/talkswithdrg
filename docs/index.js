7e8b48134e66e1507ca6b4b468389d9757a14d57
DrG <DrG@email.com>
2021-03-10 08:09:41 -0800

    Adding in the tracking file to avoid failures

--- docs/index.js
+++ docs/index.js
@@ -12,14 +12,22 @@ try {
     .map( file => fs.readFileSync( path.join( core.getInput('dir_name'), "notes", file ) ) )
     .join( "\n---\n" );
 
+  console.log( "Shareboard length: ", shareboardFull.length);
+
   console.log( "Shareboard contents:\n");
   console.log(shareboardFull);
 
-  console.log("Writing to the markdown file in the talk folder: ", core.getInput('shareboard_file'))
-  fs.writeFileSync( core.getInput('shareboard_file'), shareboardFull );
+  if(shareboardFull.length == 0) {
+    console.log("Writing random number to tracking file because no other changes")
+    fs.writeFileSync( core.getInput('tracking_file'), "Random Number: " + Math.random() );
+  }
+  else {
+    console.log("Writing to the markdown file in the talk folder: ", core.getInput('shareboard_file'))
+    fs.writeFileSync( core.getInput('shareboard_file'), shareboardFull );
 
-  console.log("Writing to the markdown file for the web: ", core.getInput('web_file'))
-  fs.writeFileSync( core.getInput('web_file'), shareboardFull );
+    console.log("Writing to the markdown file for the web: ", core.getInput('web_file'))
+    fs.writeFileSync( core.getInput('web_file'), shareboardFull );
+  }
 
 } catch (error) {
   core.setFailed(error.message);
