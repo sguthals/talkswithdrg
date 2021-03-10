bee13d75ce4e0583b2b99cb0052c7ef10643a826
Dr G <sarah+dr@guthals.com>
2021-03-10 08:18:52 -0800

    Adding in tracking file regardless of length of shareboard

--- docs/index.js
+++ docs/index.js
@@ -12,22 +12,21 @@ try {
     .map( file => fs.readFileSync( path.join( core.getInput('dir_name'), "notes", file ) ) )
     .join( "\n---\n" );
 
-  console.log( "Shareboard length: ", shareboardFull.length);
+  console.log( "tracking_file: ", core.getInput('tracking_file'));
 
   console.log( "Shareboard contents:\n");
   console.log(shareboardFull);
 
-  if(shareboardFull.length == 0) {
-    console.log("Writing random number to tracking file because no other changes")
+  if(core.getInput('tracking_file')) {
+    console.log("Writing random number to tracking file in case no other changes")
     fs.writeFileSync( core.getInput('tracking_file'), "Random Number: " + Math.random() );
   }
-  else {
-    console.log("Writing to the markdown file in the talk folder: ", core.getInput('shareboard_file'))
-    fs.writeFileSync( core.getInput('shareboard_file'), shareboardFull );
 
-    console.log("Writing to the markdown file for the web: ", core.getInput('web_file'))
-    fs.writeFileSync( core.getInput('web_file'), shareboardFull );
-  }
+  console.log("Writing to the markdown file in the talk folder: ", core.getInput('shareboard_file'))
+  fs.writeFileSync( core.getInput('shareboard_file'), shareboardFull );
+
+  console.log("Writing to the markdown file for the web: ", core.getInput('web_file'))
+  fs.writeFileSync( core.getInput('web_file'), shareboardFull );
 
 } catch (error) {
   core.setFailed(error.message);
